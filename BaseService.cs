9176a8b50f627f25d624138309c5df3166c47257
Matt Mombrea <mombrea@cypressnorth.com>
2014-02-28 12:11:16 -0500

    Create BaseService.cs

---
+++ BaseService.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace com.youapp.data.services
+{
+    public class BaseService<TObject> where TObject : class
+    {
+        protected RecipeContext _context;
+
+        public BaseService(RecipeContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<TObject> GetAll()
+        {
+            return _context.Set<TObject>().ToList();
+        }
+
+        public async Task<ICollection<TObject>> GetAllAsync()
+        {
+            return await _context.Set<TObject>().ToListAsync();
+        }
+
+        public TObject Get(int id)
+        {
+            return _context.Set<TObject>().Find(id);
+        }
+
+        public async Task<TObject> GetAsync(int id)
+        {
+            return await _context.Set<TObject>().FindAsync(id);
+        }
+
+        public TObject Find(Expression<Func<TObject, bool>> match)
+        {
+            return _context.Set<TObject>().SingleOrDefault(match);
+        }
+
+        public async Task<TObject> FindAsync(Expression<Func<TObject, bool>> match)
+        {
+            return await _context.Set<TObject>().SingleOrDefaultAsync(match);
+        }
+
+        public ICollection<TObject> FindAll(Expression<Func<TObject, bool>> match)
+        {
+            return _context.Set<TObject>().Where(match).ToList();
+        }
+
+        public async Task<ICollection<TObject>> FindAllAsync(Expression<Func<TObject, bool>> match)
+        {
+            return await _context.Set<TObject>().Where(match).ToListAsync();
+        }
+
+        public TObject Add(TObject t)
+        {
+            _context.Set<TObject>().Add(t);
+            _context.SaveChanges();
+            return t;
+        }
+
+        public async Task<TObject> AddAsync(TObject t)
+        {
+            _context.Set<TObject>().Add(t);
+            await _context.SaveChangesAsync();
+            return t;
+        }
+
+        public TObject Update(TObject updated,int key)
+        {
+            if (updated == null)
+                return null;
+
+            TObject existing = _context.Set<TObject>().Find(key);
+            if (existing != null)
+            {
+                _context.Entry(existing).CurrentValues.SetValues(updated);
+                _context.SaveChanges();
+            }
+            return existing;
+        }
+
+        public async Task<TObject> UpdateAsync(TObject updated, int key)
+        {
+            if (updated == null)
+                return null;
+
+            TObject existing = await _context.Set<TObject>().FindAsync(key);
+            if (existing != null)
+            {
+                _context.Entry(existing).CurrentValues.SetValues(updated);
+                await _context.SaveChangesAsync();
+            }
+            return existing;
+        }
+
+        public void Delete(TObject t)
+        {
+            _context.Set<TObject>().Remove(t);
+            _context.SaveChanges();
+        }
+
+        public async void DeleteAsync(TObject t)
+        {
+            _context.Set<TObject>().Remove(t);
+            await _context.SaveChangesAsync();
+        }
+
+        public int Count()
+        {
+            return _context.Set<TObject>().Count();
+        }
+
+        public async Task<int> CountAsync()
+        {
+            return await _context.Set<TObject>().CountAsync();
+        }
+    }
+}
